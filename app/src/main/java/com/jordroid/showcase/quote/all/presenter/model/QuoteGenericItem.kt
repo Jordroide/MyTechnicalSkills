89a938a318037e7755aee36450b1200057dae355
Jordroid <jordaneserreau@gmail.com>
2021-06-16 19:39:59 +0200

    refactor: rename and regroup Quote ui item

--- app/src/main/java/com/jordroid/showcase/quote/all/presenter/model/QuoteGenericItem.kt
+++
@@ -1,3 +0,0 @@
-package com.jordroid.showcase.quote.all.presenter.model
-
-interface QuoteGenericItem
--- app/src/main/java/com/jordroid/showcase/quote/all/presenter/model/QuoteItemHeader.kt
+++
@@ -1,3 +0,0 @@
-package com.jordroid.showcase.quote.all.presenter.model
-
-data class QuoteItemHeader(val animeName : String) : QuoteGenericItem
