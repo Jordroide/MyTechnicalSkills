58dca782c1ba6b1e67526c34c0da3052bece0cc3
Serreau Jordane <jordaneserreau@gmail.com>
2021-10-06 09:50:30 +0200

    chore: update gradle and library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,9 +55,9 @@ android {
 dependencies {
 
     /** Androidx dependencies */
-    implementation("androidx.activity:activity-ktx:1.3.0")
+    implementation("androidx.activity:activity-ktx:1.3.1")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     /** Google dependencies */
@@ -76,12 +76,13 @@ dependencies {
     implementation("io.insert-koin:koin-android:3.0.2")
     implementation("com.squareup.retrofit2:retrofit:2.8.0")
     implementation("com.squareup.retrofit2:converter-gson:2.8.0")
+
     /** Test dependencies */
     /** Androidx dependencies */
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     /** Test framework */
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("io.mockk:mockk:1.11.0")
     /** Kotlin dependencies */
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0")
